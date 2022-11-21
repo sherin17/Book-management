f8f5ebdc05c84a254a532bb3e802efd5e553e531
sherin17 <112325140+sherin17@users.noreply.github.com>
2022-11-21 21:44:16 +0530

    first commit

--- backend/middlewares/mongodb.js
+++ backend/middlewares/mongodb.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://Sherin17:Reenijiji1@cluster0.quvjjgb.mongodb.net/BookManagement?retryWrites=true&w=majority ', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}). then(()=>{
+mongoose.connect('mongodb+srv://Sherin17:Reenijiji1@cluster0.quvjjgb.mongodb.net/BookManagement?retryWrites=true&w=majority')
+. then(()=>{
     console.log('mongoDB connected successfully!!!')
 }).catch((error)=>{
     console.log(error.message)
